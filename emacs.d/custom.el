ce075e9f9e8b1afa7641f3ff075d1f6905631e97
Sam Brown <sambrown.w@gmail.com>
2026-03-25 22:17:09 +1030

    emacs

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,9 +5,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-operandi-tinted))
+ '(org-agenda-files
+   '("~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Phone/2026-03 March.org"))
  '(package-selected-packages
-   '(auto-dark catppuccin-theme exec-path-from-shell lua-mode magit
-	       markdown-mode)))
+   '(auto-dark catppuccin-theme consult exec-path-from-shell
+	       kanata-kbd-mode lua-mode magit marginalia markdown-mode
+	       orderless vertico))
+ '(package-vc-selected-packages
+   '((kanata-kbd-mode :url "https://github.com/chmouel/kanata-kbd-mode/"))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,11 +5,52 @@
   (load custom-file))
 
 
-;; redirect autosave files to temp dir
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; UI
+
+
+;; line numbers everywhere
+(global-display-line-numbers-mode 1)
+
+
+; Remove the splash screen
+(setq inhibit-splash-screen t)
+
+
+;; Remove menu bar and toolbar
+(customize-set-variable 'menu-bar-mode nil)
+(customize-set-variable 'tool-bar-mode nil)
+
+;; Remove scroll bars
+(customize-set-variable 'set-scroll-bar-mode nil)
+
+;; don't let the buffer line spacing be stretched
+(setq x-stretch-cursor nil)
+
+;; try different font
+;;(set-default-font "Menlo 10")
+(set-face-attribute 'default nil :height 130)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; behaviour
+
+;; eval keys
+(global-set-key "\C-\M-R" 'eval-region)
+(global-set-key "\C-\M-B" 'eval-buffer)
+
+;; Redirect backup files to temp dir
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+
+;; redirect backup files
+;; (setq auto-save-file-name-transforms
+;;      `((".*" ,temporary-file-directory t)))
+
+;; autosaves apply to actual file
+(auto-save-visited-mode 1)
+
+;; allow type y / n
+(setq use-short-answers t)
 
 
 ;; map escape key to c-g... see how it goes
@@ -23,7 +64,8 @@
 
 
 ;; which key!
-(which-key-mode 1)
+;; n.b. less useful than I thought
+;; (which-key-mode 1)
 
 
 ;; recent files!
@@ -33,17 +75,9 @@
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 
-;; line numbers everywhere
-(global-display-line-numbers-mode 1)
-
-
 ;; Update buffers when the underlying file has changed
 (global-auto-revert-mode 1)
 (setq global-auto-revert-non-file-buffers t)
-
-
-;; don't let the buffer line spacing be stretched
-(setq x-stretch-cursor nil)
 
 
 ;; eglot
@@ -70,29 +104,53 @@
 (use-package lua-mode
   :defer t
   ;; auto start eglot lsp
-  :hook (lua-mode . eglot-ensure)
-  :config (
-	   ;; default tab==3???
-	   setq lua-indent-level 4)
+  :hook
+  (lua-mode . eglot-ensure)
+  :custom
+  (lua-indent-level 4 "Make lua tab indent sane")
   )
+
 
 
 ;; catppuccin theme
 (use-package catppuccin-theme
-  :config (setq catppuccin-flavor 'macchiato))
+  :custom
+  (catppuccin-flavor 'macchiato)
+  )
 
 
 ;; auto dark
 (use-package auto-dark
   :if (eq system-type 'darwin) ;; only on macos
-  :hook (
-	 (auto-dark-dark-mode .  (lambda ()
-				   (setq catppuccin-flavor 'macchiato)
-				   (catppuccin-reload)))
-	 (auto-dark-light-mode .  (lambda ()
-				   (setq catppuccin-flavor 'latte)
-				   (catppuccin-reload))))
-  :config (
-    ignore-errors
-    (setq auto-dark-themes '((catppuccin) (catppuccin)))
-    (auto-dark-mode 1)))
+  :custom
+  (auto-dark-themes '((catppuccin) (modus-operandi-tinted)))
+  :config
+  (auto-dark-mode 1)
+  )
+	   
+
+
+;; vertico (command completion)
+(use-package vertico
+  :config
+  (vertico-mode 1)
+  )
+
+
+;; orderless (search mod)
+(use-package orderless
+  :custom
+  (completion-styles '(orderless))
+  )
+
+;; consult (completion for everything else)
+(use-package consult
+  :custom
+  (completion-in-region-function #'consult-completion-in-region))
+
+
+(use-package kanata-kbd-mode
+  :vc
+  (:url "https://github.com/chmouel/kanata-kbd-mode/" :rev 0315b56)
+  :mode
+  ("\\.kbd\\'" . kanata-kbd-mode))
